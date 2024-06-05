4f3361f779b30e25a69f88cee1c66b1104ad3f44
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-05 23:01:09 +0600

    jwt token and middleware api done

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+// jwt token middleware
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    res.status(401).send({ error: true, message: "Unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      res.status(403).send({ error: true, message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zexvqii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,6 +50,16 @@ async function run() {
     const purchaseCollection = client
       .db(`nextNatureNursery`)
       .collection("purchase");
+
+    // -------------------------jwt token api---------------------------------
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
 
     // -------------------------user api---------------------------------
     // Route to get all users
