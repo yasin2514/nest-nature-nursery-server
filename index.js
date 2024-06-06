a8ee53499529c34eeef32987e4c757b03e816e2c
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-06-07 00:16:53 +0600

    updated

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // collection
     const userCollection = client.db(`nextNatureNursery`).collection("users");
